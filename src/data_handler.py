33971da3a8d4a440a7700e70cced8d4160e16063
Murad Rakhmanov <rakhmanov.murr@gmail.com>
2024-02-17 22:23:44 +0400

    edit data_handler.py

--- src/data_handler.py
+++ src/data_handler.py
@@ -1,6 +1,5 @@
 # Libraries
-import sys # Used in ImportError 
-import math # Used to remove all NaN values
+import sys # Used in ImportError, SerialException
 
 try:
     import serial
@@ -9,18 +8,22 @@ except ImportError:
     sys.exit()
 
 # Init
-bt_data = serial.Serial('/dev/rfcomm0', 9600) # Bluetooth connection
-print("Initialization Successful")
+try:
+    bt_data = serial.Serial('/dev/rfcomm0', 9600) # Bluetooth connection
+    print("Initialization Successful")
+except serial.SerialException:
+    print("ERROR: Connect to HC05 via Bluetooth")
+    sys.exit()
 
 # Writing readings to DB
 while True:
     data = bt_data.readline().decode().strip().split()
     float_data_list = []
     for x in data:
-        float_data_list.append(float(x))
-        
-    if all(math.isnan(val) for val in float_data_list):
-        continue
+        if x == 'nan':
+            continue
+        else:
+            float_data_list.append(float(x))
 
     with open("src/database.txt", "a") as db:
         db.write(f"{float_data_list[0]} ") # Temperature
